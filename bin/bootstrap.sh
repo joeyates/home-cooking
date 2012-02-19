01d6462b22297a39150495f2dfe0a276e6bce9c1
Joe Yates <joe.g.yates@gmail.com>
2012-02-19 18:52:37 +0000

    Run chef as sudo and change file ownership explicitly

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -5,5 +5,5 @@ cd .install
 rm -rf home-cooking
 git clone git://github.com/joeyates/home-cooking.git
 cd home-cooking
-chef-solo -c chef-solo.rb -j attributes.js
+sudo chef-solo -c chef-solo.rb -j attributes.js -u root
 
--- cookbooks/user/recipes/default.rb
+++ cookbooks/user/recipes/default.rb
@@ -3,6 +3,8 @@ HOME="/home/#{ node[:user][:name] }"
 %w(.bashrc .gemrc .gitconfig .gitexcludes .irbrc .rvmrc .screenrc .zshrc).each do | rc |
   template "#{ HOME }/#{ rc }" do
     mode '0644'
+    owner 'joe'
+    group 'joe'
   end
 end
 
