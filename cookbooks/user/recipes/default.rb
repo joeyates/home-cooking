c165e8ea2308588910a7d8a1d9ea02d680f9e644
Joe Yates <joe.g.yates@gmail.com>
2012-03-17 15:51:31 +0000

    Added SHA1 stamping for installed files

--- cookbooks/user/recipes/default.rb
+++ cookbooks/user/recipes/default.rb
@@ -1,10 +1,23 @@
-# TODO: Manage SHA-1s
-HOME="/home/#{ node[:user][:name] }"
+lib_directory = File.expand_path( '../../../lib', File.dirname( __FILE__ ) )
+$LOAD_PATH.unshift( lib_directory )
+
+require 'home_cooking'
+
+HOME = "/home/#{ node[:user][:name] }"
+
 %w(.bashrc .gemrc .gitconfig .gitexcludes .irbrc .rvmrc .screenrc .zshrc).each do | rc |
-  template "#{ HOME }/#{ rc }" do
+  destination = "#{ HOME }/#{ rc }"
+  template destination do
     mode '0644'
     owner 'joe'
     group 'joe'
+  end
+
+  file = HomeCooking::File.new( destination )
+  if file.modified?
+    puts "File '#{ destination }' has been modified"
+  else
+    file.stamp! unless file.stamped?
   end
 end
 
---
+++ lib/home_cooking/file.rb
@@ -0,0 +1,62 @@
+require 'digest/sha1'
+require 'pp'
+
+module HomeCooking
+
+  class File
+
+    SHA1_STAMP  = 'HOME-COOKING-SHA1'
+    STAMP_MATCH = Regexp.new( "^# #{ SHA1_STAMP }" )
+
+    def initialize( path_name )
+      @path_name = path_name
+      @lines     = File.open( @path_name ).map { | l | l }
+    end
+
+    def modified?
+      i           = stamp_line
+      return true if i.nil?
+
+      current_sha = @lines[ i ][ /^# #{ SHA1_STAMP } (.*)/, 1]
+      puts "current_sha: #{ current_sha }"
+      clean     = @lines.select { | line | line !~ STAMP_MATCH }
+      clean_sha = sha( clean.join )
+      puts "clean_sha: #{ clean_sha }"
+
+      current_sha != clean_sha
+    end
+
+
+    def stamped?
+      ! stamp_line.nil?
+    end
+
+    def sha( text )
+      Digest::SHA1.hexdigest( text )
+    end
+
+    def stamp!
+      puts "stamping #{ @path_name }"
+      @lines.reject! { | line | line =~ STAMP_MATCH } 
+      # Ensure there's a final newline
+      if lines[ -1 ] !~ /\n$/
+        lines[ -1 ] += "\n"
+      end
+
+      text = lines.join
+      sha1 = sha( text )
+      text += "# #{ SHA1_STAMP } #{ sha1 }\n"
+
+      File.open( @path_name, 'w' ) { | f | f.write text }
+    end
+
+    private
+
+    def stamp_line
+      @lines.find_index { | line | line =~ STAMP_MATCH }
+    end
+
+  end
+
+end
+
