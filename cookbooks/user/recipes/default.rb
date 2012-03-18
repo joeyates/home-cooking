826279b8daec1d189617a7a010e92c9b269a03dc
Joe Yates <joe.g.yates@gmail.com>
2012-03-18 17:40:50 +0000

    In order for ruby code to be avaluated during convergence, it needs to be in a ruby_block

--- cookbooks/user/recipes/default.rb
+++ cookbooks/user/recipes/default.rb
@@ -8,18 +8,21 @@ HOME = "/home/#{ node[:user][:name] }"
 %w(.bashrc .gemrc .gitconfig .gitexcludes .irbrc .rvmrc .screenrc .zshrc).each do | rc |
   destination = "#{ HOME }/#{ rc }"
   template destination do
-    action :create_if_missing
     mode '0644'
     owner 'joe'
     group 'joe'
   end
 
-  file = HomeCooking::File.new( destination )
-  if ! file.stamped?
-    puts "File '#{ destination }' is not stamped"
-    file.stamp!
-  elsif file.modified?
-    puts "File '#{ destination }' has been modified"
+  ruby_block "apply_home_cooking_sha1_stamp" do
+    block do
+      file = HomeCooking::File.new( destination )
+      if ! file.stamped?
+        puts "File '#{ destination }' is not stamped"
+        file.stamp!
+      elsif file.modified?
+        puts "File '#{ destination }' has been modified"
+      end
+    end
   end
 end
 
